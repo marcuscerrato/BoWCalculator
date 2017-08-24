a7a0ace1dc9e256702744493f158f496b61983c2
kishi <kishi@gsxr>
2017-08-23 21:09:11 -0300

    To work @ home.

--- src/main/java/intermidia/BoWCalculator/BoWCalculator.java
+++ src/main/java/intermidia/BoWCalculator/BoWCalculator.java
@@ -1,9 +1,24 @@
 package intermidia.BoWCalculator;
 
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
 public class BoWCalculator 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        
+    	//StringToWordVector bowFilter = new StringToWordVector(500);
+    	DataSource dataSource = new DataSource(args[0]);    	
+    	Instances instances = dataSource.getDataSet();
+    	System.out.println("Shot \t Text");
+    	int i = 0;
+    	for(Instance inst : instances)
+    	{
+    		i++;
+        	System.out.println(inst.attribute(1));
+    	}
+    	System.out.println("Inst qty: " + i);
     }
 }
